fdc55204dc3bf42c27cf64eac1b65cf229a99326
frank <519580205@qq.com>
2026-02-11 15:51:01 +0800

    openfeign部分

---
+++ cloud-api-commons/src/main/java/com/atguigu/cloud/apis/PayFeignApi.java
@@ -0,0 +1,24 @@
+package com.atguigu.cloud.apis;
+
+import com.atguigu.cloud.entities.PayDTO;
+import com.atguigu.cloud.resp.ResultData;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient("cloud-payment-service")
+public interface PayFeignApi {
+
+    @PostMapping(value = "/pay/add")
+    public ResultData addPay(@RequestBody PayDTO pay);
+
+    @GetMapping(value = "/pay/get/{id}")
+    public ResultData getById(@PathVariable("id") Integer id);
+
+    @GetMapping("/pay/get/info")
+    public ResultData mylb();
+
+}
